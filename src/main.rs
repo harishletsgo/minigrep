1479c1b9b2b4f2e76996f35af6ac12c46325d08e
Harish Prasanna <harishprasanna@ieee.org>
2020-06-21 19:14:30 -0400

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
+use minigrep::*;
 use std::env;
-use std::fs;
-
-struct Config{
-	query: String,
-	filename: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-	let query = args[1].clone();
-	let filename = args[2].clone();
-
-	Config{query, filename}
-}
+use std::process;
 
 fn main() {
 	// Store all arguments from the env::args() iterator by converting it to a vector using collect()
@@ -19,12 +8,13 @@ fn main() {
 	let args: Vec<String> = env::args().collect();
 
 	// Store the search string and input file name
-	let config = parse_config(&args);
+	let config = Config::new(&args).unwrap_or_else(|err|{
+		eprintln!("Problem parsing args: {}", err);
+		process::exit(1);
+	});
 
-	println!("Searching for {}", config.query);
-	println!("In file {}", config.filename);
-
-	let contents = fs::read_to_string(config.filename).expect("Something wrong with file");
-
-	println!("With text:\n{}", contents);
+	if let Err(e) = run(config){
+		eprintln!("App Error: {}", e);
+		process::exit(1);
+	}
 }
